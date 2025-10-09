aac923bc4e72b61319472ab9648149df03ac7253
S3b4s-117 <a24310350@live.ceti.mx>
2025-10-09 02:00:02 +0000

    sizeof

--- src/punteros.cpp
+++ src/punteros.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int main () {
-    int *pNum =NULL;
+int main (int argc, char const *argv[])
+{
+    int *pNumeroNuevo =NULL;
 
-    pNum = new int;
+    pNumeroNuevo = new int;
     
-    *pNum = 25;
+    *pNumeroNuevo = 25;
 
+std :: cout <<"direccion : " <<pNumeroNuevo << '\n';
+std :: cout <<"valor: " << *pNumeroNuevo << '\n';
 
-std :: cout <<"direccion : " <<pNum << '\n';
-std :: cout <<"valor: " << pNum << '\n';
-std :: cout <<"valor: " << *pNum << '\n';
-std :: cout <<"valor: " << &pNum << '\n';
+cout<< "Tipos de datos : " << endl;
+cout << "Tamanño int: " << sizeof (int) <<endl;
+cout << "Tamanño long: " << sizeof (long) <<endl;
+cout << "Tamanño float: "<< sizeof (float) <<endl;
+cout << "Tamanño double: "<< sizeof (double) <<endl;
+cout << "Tamanño char: "<< sizeof (char) <<endl;
+cout << "Tamanño bool: "<< sizeof (bool) <<endl;
+cout << "Tamanño byte: "<< sizeof (byte) <<endl;
 return 0;
 }
